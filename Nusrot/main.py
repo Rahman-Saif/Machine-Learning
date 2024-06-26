3d3175cbbd789eff20f9e5116b43263797c4acbc
Saif Rahman <rahmansaif.413@gmail.com>
2024-06-26 19:40:45 +0600

    Add files via upload

---
+++ Nusrot/main.py
@@ -0,0 +1,10 @@
+# !pip install pandas
+# !pip install pandas-profiling
+
+import pandas as pd
+from pandas_profiling import ProfileReport 
+df=pd.read_csv('train.csv')
+print(df)
+
+profile=ProfileReport(df)
+profile.to_file(output_file='train.html')
